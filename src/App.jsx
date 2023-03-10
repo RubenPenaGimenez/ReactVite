161707222ac9d80e918d2a2bef50773e6ebd5214
MONLAU\rubenpengim <rubenpengim@monlau.com>
2023-03-10 20:53:48 +0100

    cambiado x2

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,10 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello my name is Rubén Peña!! From 2 CFGS DAW</h1>
+      <h1>Hello my name is Rubén Peña!! From 2 CFGS</h1>
+      <div class="spinner-border" role="status">
+        <img src="../assets/img/holasoygerman.png"></img>
+      </div>
     </div>
   )
 }
